d791fb50989202a44c8252b1df1c160c9f2b3ea5
adhamsteve213 <adhamsteve21@gmail.com>
2025-10-16 14:21:36 +0300

    Delete src/components directory

--- src/components/ExampleCarouselImage.jsx
+++
@@ -1,8 +0,0 @@
-// Create this file: src/components/ExampleCarouselImage.jsx
-import carousel from '../assets/carousel.js';
-
-const ExampleCarouselImage = ({ src, alt }) => (
-  <img src={carousel.image} alt={alt} style={{display : 'block', width : '100%'}}/>
-);
-
-export default ExampleCarouselImage;
